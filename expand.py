cc1dd860778a32ba341a3dc5f6695a9f55461e04
Jake Vasilakes <jvasilakes@gmail.com>
2014-04-20 20:02:23 -0500

    Update expand.py

--- expand.py
+++ expand.py
@@ -38,12 +38,17 @@ def fill(corners_list):
     vicinity = []
 
 
+    # for each corner in corners_list, w/o the final value: [A, B, C, D]
     for i in xrange((len(corners_list) - 1)):
 
+	# E.g. if y value of A is equal to y value of B,
+	# then we are dealing with a horizontal line from A to B
 	if corners_list[i][0] == corners_list[i+1][0]:
 
 	    y = corners_list[i][0]
 
+	    # if x value of B is greater than the x value of A,
+	    # then we are moving left to right (A is to the left of B)
 	    if corners_list[i+1][1] > corners_list[i][1]:
 	        for x in xrange(corners_list[i][1], corners_list[i+1][1]):
 	            vicinity.append([y, x])
